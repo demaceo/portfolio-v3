1e960ef6736acff04b1628e80def050430e94b50
demaceo <hdemaceo@gmail.com>
2025-07-17 18:49:42 -0600

    update build script to copy client files to dist and add vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
+  ssr: {
+    noExternal: ["react-router-dom"],
   },
 });
